2c4d5b4eea6467635f65da3f0e7b0d59fdf98d2c
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-07-25 11:02:40 -0400

    commented out all of appp.js and moved code to app.js

--- assets/javascrpt/app.js
+++ assets/javascrpt/app.js
@@ -27,6 +27,81 @@ var playerPickTwo = "";
 
 
 
+firebase.auth().onAuthStateChanged(function(user){
+  console.log(user);
+
+  if(user){
+    console.log("logged in as: " + user.email);
+  } else {
+    console.log(user.email + " is logged out");
+  }
+})
+
+
+
+// modals
+document.addEventListener("DOMContentLoaded", function () {
+  var modals = document.querySelectorAll(".modal");
+  M.Modal.init(modals);
+})
+
+// sign in
+var signIn = $("#signup-form")
+
+$(signIn).submit(function (event) {
+  event.preventDefault();
+
+  var email = $("#signup-email").val().trim();
+  var password = $("#signup-password").val().trim();
+
+  console.log(email);
+  console.log(password);
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(function (response) {
+    
+    console.log(response.user);
+    console.log("logged in")
+
+    var closeModal = $("#modal-signup");
+    M.Modal.getInstance(closeModal).close();
+
+    document.getElementById("signup-form").reset();
+  });
+});
+
+// sign out
+
+var signOut = $("#logout");
+
+$(signOut).click(function (event){
+  event.preventDefault();
+  
+  firebase.auth().signOut().then(() => {
+    console.log("signed out");
+  });
+});
+
+// login
+var login = $("#login-form");
+
+$(login).submit(function(event){
+  event.preventDefault();
+
+  var email = $("#login-email").val().trim();
+  var password = $("#login-password").val().trim();
+
+  firebase.auth().signInWithEmailAndPassword(email, password).then(function(response){
+    console.log(response.user);
+  
+    var closeModal = $("#modal-login");
+    M.Modal.getInstance(closeModal).close();
+
+    document.getElementById("login-form").reset();
+  });
+});
+
+
+
 
 
 
@@ -37,7 +112,7 @@ $(document).on("click", "#submitChoice2", function (event) {
   playerPickOne = $("#playerOneChoice").val().trim();
   playerPickTwo = $("#playerTwoChoice").val().trim();
 
-  dataRef.push({
+  dataRef.update({
     player1: playerPickOne,
     player2: playerPickTwo
   });
--- assets/javascrpt/apppp.js
+++ assets/javascrpt/apppp.js
@@ -1,72 +1,74 @@
-firebase.auth().onAuthStateChanged(function(user){
-  console.log(user);
-
-  if(user){
-    console.log("logged in as: " + user.email);
-  } else {
-    console.log(user.email + " is logged out");
-  }
-})
 
 
+// firebase.auth().onAuthStateChanged(function(user){
+//   console.log(user);
 
-// modals
-document.addEventListener("DOMContentLoaded", function () {
-  var modals = document.querySelectorAll(".modal");
-  M.Modal.init(modals);
-})
+//   if(user){
+//     console.log("logged in as: " + user.email);
+//   } else {
+//     console.log(user.email + " is logged out");
+//   }
+// })
 
-// sign in
-var signIn = $("#signup-form")
 
-$(signIn).submit(function (event) {
-  event.preventDefault();
 
-  var email = $("#signup-email").val().trim();
-  var password = $("#signup-password").val().trim();
+// // modals
+// document.addEventListener("DOMContentLoaded", function () {
+//   var modals = document.querySelectorAll(".modal");
+//   M.Modal.init(modals);
+// })
 
-  console.log(email);
-  console.log(password);
+// // sign in
+// var signIn = $("#signup-form")
 
-  firebase.auth().createUserWithEmailAndPassword(email, password).then(function (response) {
+// $(signIn).submit(function (event) {
+//   event.preventDefault();
+
+//   var email = $("#signup-email").val().trim();
+//   var password = $("#signup-password").val().trim();
+
+//   console.log(email);
+//   console.log(password);
+
+//   firebase.auth().createUserWithEmailAndPassword(email, password).then(function (response) {
     
-    console.log(response.user);
-    console.log("logged in")
+//     console.log(response.user);
+//     console.log("logged in")
 
-    var closeModal = $("#modal-signup");
-    M.Modal.getInstance(closeModal).close();
+//     var closeModal = $("#modal-signup");
+//     M.Modal.getInstance(closeModal).close();
 
-    document.getElementById("signup-form").reset();
-  });
-});
+//     document.getElementById("signup-form").reset();
+//   });
+// });
 
-// sign out
+// // sign out
 
-var signOut = $("#logout");
+// var signOut = $("#logout");
 
-$(signOut).click(function (event){
-  event.preventDefault();
+// $(signOut).click(function (event){
+//   event.preventDefault();
   
-  firebase.auth().signOut().then(() => {
-    console.log("signed out");
-  });
-});
+//   firebase.auth().signOut().then(() => {
+//     console.log("signed out");
+//   });
+// });
 
-// login
-var login = $("#login-form");
+// // login
+// var login = $("#login-form");
 
-$(login).submit(function(eevent){
-  event.preventDefault();
+// $(login).submit(function(event){
+//   event.preventDefault();
 
-  var email = $("#login-email").val().trim();
-  var password = $("#login-password").val().trim();
+//   var email = $("#login-email").val().trim();
+//   var password = $("#login-password").val().trim();
 
-  firebase.auth().signInWithEmailAndPassword(email, password).then(function(response){
-    console.log(response.user);
+//   firebase.auth().signInWithEmailAndPassword(email, password).then(function(response){
+//     console.log(response.user);
   
-    var closeModal = $("#modal-login");
-    M.Modal.getInstance(closeModal).close();
+//     var closeModal = $("#modal-login");
+//     M.Modal.getInstance(closeModal).close();
 
-    document.getElementById("login-form").reset();
-  })
-})
+//     document.getElementById("login-form").reset();
+//   });
+// });
