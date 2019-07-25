a37127f7747069d8010a52b3c8bc39a4ba76b923
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-07-25 01:22:29 -0400

    added nav bar, sign in auth form function, if/else statement to check player's guess

--- assets/javascrpt/app.js
+++ assets/javascrpt/app.js
@@ -10,8 +10,9 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-var dataRef = firebase.database();
-var gameRef = firebase.database().ref().child("rpsGame")
+var dataRef = firebase.database().ref();
+// var gameRef = firebase.database().ref().child()
+
 
 var wins1 = 0;
 var loss1 = 0;
@@ -25,31 +26,80 @@ var playerPickOne = "";
 var playerPickTwo = "";
 
 
-  $(document).on("click", "#submitChoice", function (event) {
-    
-    event.preventDefault();
 
-    playerPickOne = $("#playerOneChoice").val().trim();
 
-    gameRef.push({
-      player1: playerPickOne
-    });
+
+
+$(document).on("click", "#submitChoice2", function (event) {
+
+  event.preventDefault();
+
+  playerPickOne = $("#playerOneChoice").val().trim();
+  playerPickTwo = $("#playerTwoChoice").val().trim();
+
+  dataRef.push({
+    player1: playerPickOne,
+    player2: playerPickTwo
   });
 
+  // dataRef.update({
+  //   player1: playerPickOne,
+  //   player2: playerPickTwo
+  // })
 
-  $(document).on("click", "#submitChoice2", function (event) {
-    
-    event.preventDefault();
+});
 
-    playerPickTwo = $("#playerTwoChoice").val().trim();
 
-    gameRef.push({
-    player2: playerPickTwo
-  })
-})
 
-gameRef.on("child_added", function(childSnapshot){
+
+
+
+// $(document).on("click", "#submitChoice2", function (event) {
+
+//   event.preventDefault();
+
+//   playerPickTwo = $("#playerTwoChoice").val().trim();
+
+//   dataRef.push({
+//     player2: playerPickTwo
+//   })
+// })
+
+dataRef.on("child_added", function (childSnapshot) {
   console.log(childSnapshot.val().player1);
   console.log(childSnapshot.val().player2);
 
+  var check1 = childSnapshot.val().player1;
+  var check2 = childSnapshot.val().player2
+
+  $("#pick1").text("Player One's Pick: " + check1)
+  $("#pick2").text("Player Two's Pick: " + check2)
+
+  if (check1 === check2) {
+    tie++
+    $("#tie1").text("tie: " + tie)
+    $("#tie2").text("tie: " + tie)
+  }
+  else if (check1 == "rock" && check2 == "scissors" || check1 == "scissors" && check2 == "paper" || check1 == "paper" && check2 == "rock") {
+    wins1++
+    loss2++
+
+    $("#wins1").text("wins: " + wins1)
+    $("#losses2").text("losses: " + loss2)
+  }
+  else {
+    loss1++
+    $("#losses1").text("losses: " + loss1)
+
+    wins2++
+    $("#wins2").text("wins: " + wins2)
+
+    console.log("works")
+  }
+
+
+
 })
+
+
+
--- assets/javascrpt/apppp.js
+++ assets/javascrpt/apppp.js
@@ -1,142 +1,25 @@
-if
-  if (
-    ((click1).val() === "r" && (click2).val() === "s")
-    || ((click1).val() === "p" && (click2).val() === "r")
-    || ((click1).val() === "s" && (click2).val() === "p")
-    ) {
 
-    wins1++
-    $("#wins1").text("wins: " + wins1)
-
-    loss2++
-    $("#losses2").text("losses: " + loss2)
-  }
-  else if ((click1).val() === (click2).val()) {
-    tie++
-    $("#tie1").text("ties: " + ties)
-    $("#tie2").text("ties: " + ties)
-  } else {
-    losses1++
-    $("#losses1").text("losses: " + losses1)
-  }
-
-stop game
-stopGame (function () {
-  if (wins1 || wins2 === 10) {
-    alert(player1 + " is the winner") || alert(player2 + " is the winner")
-    alert("refresh to play again")
-    
-  }
-
+// modals
+document.addEventListener("DOMContentLoaded", function () {
+  var modals = document.querySelectorAll(".modal");
+  M.Modal.init(modals);
 })
 
-// }
+// sign in
+var signIn = $("#signup-form")
 
-// $("#pick1").text("player one pick: " + $(this).val());
-    // var startPlayer = 1;
+$(signIn).submit(function (event) {
+  event.preventDefault();
 
-// player two pick
-// function playerTwo() {
-  // $(".rps2").on("click", function playerTwo() {
-  //   $("#pick2").text("player two pick: " + $(this).val());
-  //   var startPlayer = 2;
-    // store var
-  // })
-// };
+  var email = $("#signup-email").val().trim();
+  var password = $("#signup-password").val().trim();
 
-// game function
-// function game() {
-//   playerOne();
-//   playerTwo();
-// }
+  console.log(email)
+  console.log(password)
 
-// if (startPlayer === 2) {
-  // data from firebase
-  // if else of who picked what letter
+  firebase.auth().createUserWithEmailAndPassword(email, password).then(function (response) {
+    
+    console.log(response.user)
 
-// }
-// in firebase - player object
-// wins1, losses, choice
-// chat username
-// player.choice pick update each round
-
-
-
-
-
-
-
-
-
-
-
-
-    firebase
-    var firebaseConfig = {
-        apiKey: "AIzaSyAoUmi01K2fvQpFeo-LXBqQgNTikWYeHWo",
-        authDomain: "rps-multiplayer-c0a89.firebaseapp.com",
-        databaseURL: "https://rps-multiplayer-c0a89.firebaseio.com",
-        projectId: "rps-multiplayer-c0a89",
-        storageBucket: "",
-        messagingSenderId: "272026270134",
-        appId: "1:272026270134:web:96dc14684d8115e3"
-    }
-
-    firebase.initializeApp(firebaseConfig);
-
-    var database = firebase.database()
-
-
-
-
-
-
-    // real one
-    // real one
-
-    var wins1 = 0;
-var loss1 = 0;
-
-var wins2 = 0;
-var loss2 = 0;
-
-var tie = 0;
-
-var startPlayer = null
-
-var firebaseConfig = {
-  apiKey: "AIzaSyAoUmi01K2fvQpFeo-LXBqQgNTikWYeHWo",
-  authDomain: "rps-multiplayer-c0a89.firebaseapp.com",
-  databaseURL: "https://rps-multiplayer-c0a89.firebaseio.com",
-  projectId: "rps-multiplayer-c0a89",
-  storageBucket: "",
-  messagingSenderId: "272026270134",
-  appId: "1:272026270134:web:96dc14684d8115e3"
-}
-
-firebase.initializeApp(firebaseConfig);
-
-var dataRef = firebase.database();
-
-var rock1 = "rockPick1";
-var paper1 = "paperPick1";
-var scissors1 = "scissorsPick1";
-
-  $(".rps1").on("click", function (event) {
-    event.preventDefault();
-
-    rock1 = $("#r1").val();
-    paper1 = $("#p1").val();
-    scissors1 = $("#s1").val();
-
-    dataRef.ref().push({
-      rock1: rock1,
-      paper1: paper1,
-      scissors1: scissors1
-    });
   })
-
-
-
-
-
+})
