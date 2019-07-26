0098794d1a28058f9b3f300d9ac8edb183465987
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-07-25 21:40:08 -0400

    moved appp.js to local desktop

--- assets/javascrpt/app.js
+++ assets/javascrpt/app.js
@@ -11,8 +11,6 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 var dataRef = firebase.database().ref();
-// var gameRef = firebase.database().ref().child()
-
 
 var wins1 = 0;
 var loss1 = 0;
@@ -24,85 +22,6 @@ var tie = 0;
 
 var playerPickOne = "";
 var playerPickTwo = "";
-
-
-
-// firebase.auth().onAuthStateChanged(function(user){
-//   console.log(user);
-
-//   if(user){
-//     console.log("logged in as: " + user.email);
-//   } else {
-//     console.log(user.email + " is logged out");
-//   }
-// })
-
-
-
-// modals
-// document.addEventListener("DOMContentLoaded", function () {
-//   var modals = document.querySelectorAll(".modal");
-//   M.Modal.init(modals);
-// })
-
-// // sign in
-// var signIn = $("#signup-form")
-
-// $(signIn).submit(function (event) {
-//   event.preventDefault();
-
-//   var email = $("#signup-email").val().trim();
-//   var password = $("#signup-password").val().trim();
-
-//   console.log(email);
-//   console.log(password);
-
-//   firebase.auth().createUserWithEmailAndPassword(email, password).then(function (response) {
-    
-//     console.log(response.user);
-//     console.log("logged in")
-
-//     var closeModal = $("#modal-signup");
-//     M.Modal.getInstance(closeModal).close();
-
-//     document.getElementById("signup-form").reset();
-//   });
-// });
-
-// // sign out
-
-// var signOut = $("#logout");
-
-// $(signOut).click(function (event){
-//   event.preventDefault();
-  
-//   firebase.auth().signOut().then(() => {
-//     console.log("signed out");
-//   });
-// });
-
-// login
-// var login = $("#login-form");
-
-// $(login).submit(function(event){
-//   event.preventDefault();
-
-//   var email = $("#login-email").val().trim();
-//   var password = $("#login-password").val().trim();
-
-//   firebase.auth().signInWithEmailAndPassword(email, password).then(function(response){
-//     console.log(response.user);
-  
-//     var closeModal = $("#modal-login");
-//     M.Modal.getInstance(closeModal).close();
-
-//     document.getElementById("login-form").reset();
-//   });
-// });
-
-
-
-
 
 
 $(document).on("click", "#submitChoice2", function (event) {
@@ -117,28 +36,7 @@ $(document).on("click", "#submitChoice2", function (event) {
     player2: playerPickTwo
   });
 
-  // dataRef.update({
-  //   player1: playerPickOne,
-  //   player2: playerPickTwo
-  // })
-
 });
-
-
-
-
-
-
-// $(document).on("click", "#submitChoice2", function (event) {
-
-//   event.preventDefault();
-
-//   playerPickTwo = $("#playerTwoChoice").val().trim();
-
-//   dataRef.push({
-//     player2: playerPickTwo
-//   })
-// })
 
 dataRef.on("child_added", function (childSnapshot) {
   console.log(childSnapshot.val().player1);
